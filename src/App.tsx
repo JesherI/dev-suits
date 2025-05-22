2af4911d799a3c1bcfa156c8f83dc55dbdb26511
Jeshersin <jeshermendieta@gmail.com>
2025-05-22 12:10:33 -0600

    0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <>
     <div className="App">
-      <h1>Welcome to DEV-SUITS</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to DEV-SUITS</h1>
     </div>
     </>
   )
