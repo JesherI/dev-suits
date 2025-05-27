6b4d1f7987e18ff454ad3a17cd2f84173c1a9557
Jeshersin <jeshermendieta@gmail.com>
2025-05-27 10:31:26 -0600

    0.1.6

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-
+import '../views/Home.css';
 export default function Layout() {
     return (
         <div>
